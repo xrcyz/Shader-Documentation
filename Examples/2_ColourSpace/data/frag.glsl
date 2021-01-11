6df736c0e2dc7e8a54de459f3ba03b50e20f8b92
xrcyz <66832355+xrcyz@users.noreply.github.com>
2021-01-11 18:16:05 +0800

    Update frag.glsl

--- Examples/2_ColourSpace/data/frag.glsl
+++ Examples/2_ColourSpace/data/frag.glsl
@@ -9,6 +9,19 @@ precision highp int;
 uniform vec2 dimensions;
 uniform vec2 mousePos;
 
+/*
+  [Iñigo Quiles](https://www.shadertoy.com/view/MsS3Wc)
+  via [Book of Shaders](https://thebookofshaders.com/06/)
+*/
+vec3 hsb2rgb( in vec3 c ){
+    vec3 rgb = clamp(abs(mod(c.x*6.0+vec3(0.0,4.0,2.0),
+                             6.0)-3.0)-1.0,
+                     0.0,
+                     1.0 );
+    rgb = rgb*rgb*(3.0-2.0*rgb);
+    return c.z * mix(vec3(1.0), rgb, c.y);
+}
+
 void main()
 {
   vec2 pos = gl_FragCoord.xy / dimensions;
@@ -24,15 +37,3 @@ void main()
   gl_FragColor = vec4(rgb, 1.0);
 }
 
-/*
-  [Iñigo Quiles](https://www.shadertoy.com/view/MsS3Wc)
-  via [Book of Shaders](https://thebookofshaders.com/06/)
-*/
-vec3 hsb2rgb( in vec3 c ){
-    vec3 rgb = clamp(abs(mod(c.x*6.0+vec3(0.0,4.0,2.0),
-                             6.0)-3.0)-1.0,
-                     0.0,
-                     1.0 );
-    rgb = rgb*rgb*(3.0-2.0*rgb);
-    return c.z * mix(vec3(1.0), rgb, c.y);
-}
