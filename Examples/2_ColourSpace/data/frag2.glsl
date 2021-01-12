a6b1d9a06bf58641607a608b2dced498c17e6bde
xrcyz <66832355+xrcyz@users.noreply.github.com>
2021-01-12 09:52:37 +0800

    add color ribbon

---
+++ Examples/2_ColourSpace/data/frag2.glsl
@@ -0,0 +1,27 @@
+#ifdef GL_ES
+precision highp float;
+precision highp int;
+#endif
+
+uniform vec2 dimensions;
+
+/*
+  [Iñigo Quiles](https://www.shadertoy.com/view/MsS3Wc)
+  via [Book of Shaders](https://thebookofshaders.com/06/)
+*/
+vec3 hsb2rgb( in vec3 c ){
+    vec3 rgb = clamp(abs(mod(c.x*6.0+vec3(0.0,4.0,2.0),
+                             6.0)-3.0)-1.0,
+                     0.0,
+                     1.0 );
+    rgb = rgb*rgb*(3.0-2.0*rgb);
+    return c.z * mix(vec3(1.0), rgb, c.y);
+}
+
+void main()
+{
+  vec2 pos = gl_FragCoord.xy / dimensions;
+  float hue = pos.y;
+  vec3 rgb = hsb2rgb(vec3(hue, 1.0, 1.0));
+  gl_FragColor = vec4(rgb, 1.0);
+}
